fcc4e851b91152d27486c5a421cc6389d73f2c06
Decompressor <johndoe@example.com>
2024-02-21 16:54:49 +0300

    added hash calculation and mine methods

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,5 +1,13 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Block struct {
 	payload []byte
 	// previous block hash, will use it for integrity check
@@ -16,4 +24,34 @@ type BlockOperations interface {
 	setHash()
 	calculateHash() string
 	mine(difficulty int)
+}
+
+func (b *Block) setHash() {
+	b.hash = b.calculateHash()
+}
+
+// Вычисление хеша текущего блока
+func (b *Block) calculateHash() string {
+	var buf []byte
+	buf = append(buf, b.previousHash...)
+	buf = append(buf, b.payload...)
+	buf = binary.LittleEndian.AppendUint64(buf, uint64(b.timestamp))
+	buf = append(buf, strconv.Itoa(b.pow)...)
+	return fmt.Sprintf("%x", sha256.Sum256(buf))
+}
+
+// В зависимости от установленной сложности на блокчейне выполняем вычисление хеша
+// проверяем, что хеш начинается с нужного количества нулей
+// если нет, то продолжаем выполнение хеширования
+// pow показывает количество попыток, которые были произведены для получения hash текущего блока
+func (b *Block) mine(difficulty int) {
+	if difficulty == 0 {
+		b.pow = 0
+		b.setHash()
+		return
+	}
+	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) {
+		b.pow++
+		b.setHash()
+	}
 }
