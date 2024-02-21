a5eaac1bed8db37baac9392c51347593efb683eb
Decompressor <johndoe@example.com>
2024-02-21 16:57:08 +0300

    blockchain interface implementation

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,5 +1,7 @@
 package blockchain
 
+import "time"
+
 // BLOCKCHAIN
 
 type Blockchain struct {
@@ -14,4 +16,55 @@ type Operations interface {
 	AddBlock(data []byte)
 	IsValid() bool
 	Size() int
+}
+
+func (c *Blockchain) AddBlock(data []byte) {
+	// получаем последний блок для сохранения предыдущего hash в новом блоке
+	lastBlock := c.chain[len(c.chain)-1]
+	newBlock := Block{
+		payload:      data,
+		previousHash: lastBlock.hash,
+		timestamp:    time.Now().UTC().UnixNano(),
+	}
+
+	// применяем майнинг к новому блоку, вычисляется pow и hash
+	// при нулевой сложности просто вычисляем hash
+	newBlock.mine(c.difficulty)
+	c.chain = append(c.chain, newBlock)
+}
+
+func (c *Blockchain) IsValid() bool {
+	for i := range c.chain[1:] {
+		prevBlock := c.chain[i]
+		currentBlock := c.chain[i+1]
+		if prevBlock.hash != currentBlock.previousHash || currentBlock.hash != currentBlock.calculateHash() {
+			return false
+		}
+	}
+	return true
+}
+
+func (c *Blockchain) Size() int {
+	return len(c.chain)
+}
+
+func CreateBlockchain(data []byte, difficulty int) Blockchain {
+	initial := Block{
+		payload:      data,
+		previousHash: "0",
+		timestamp:    time.Now().UTC().UnixNano(),
+	}
+	if difficulty == 0 {
+		initial.pow = 0
+		initial.setHash()
+	} else {
+		// майним блок, устанавливаем hash
+		initial.mine(difficulty)
+	}
+
+	return Blockchain{
+		initial:    initial,
+		chain:      []Block{initial},
+		difficulty: difficulty,
+	}
 }
